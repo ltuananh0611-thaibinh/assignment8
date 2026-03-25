fedb86a71280efbf799e13b48f3e86e7adfb502e
ltuananh0611-thaibinh <ltuananh0611@gmail.com>
2026-03-25 11:52:42 +0700

    draf3

--- bai2.py
+++ bai2.py
@@ -1,1 +1,9 @@
-print("Hello WOrl!")
+class Building:
+    def __init__(self, bottom, top, num_elevators):
+        self.elevators = []
+        for i in range(num_elevators):
+            self.elevators.append(Elevator(bottom, top))
+
+    def run_elevator(self, elevator_number, destination):
+        print(f"\nRunning elevator {elevator_number} to floor {destination}")
+        self.elevators[elevator_number].go_to_floor(destination)
